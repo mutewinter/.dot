058f4de5d55d0aaaf60fe499075c41ad40c8e07b
Jeremy Mack <jeremy@pileofturtles.com>
2012-05-06 22:13:46 -0400

    Added alias for mvim if it exists

--- aliases.sh
+++ aliases.sh
@@ -2,3 +2,8 @@
 
 alias zshrc="$EDITOR ~/dot_files/_zshrc"
 alias vimrc="$EDITOR ~/.vim/vimrc"
+
+# Use MacVim for the terminal if it's installed.
+if [ -x /usr/local/bin/mvim ]; then
+  alias vim="mvim -v"
+fi
