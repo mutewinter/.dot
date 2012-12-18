0fd18268c1f8dad2fd5161e6089eaaa365b137c5
Jeremy Mack <mack@heysparkbox.com>
2012-12-18 10:08:37 -0500

    Added shortcut cd commands

--- aliases.sh
+++ aliases.sh
@@ -1,9 +1,15 @@
 # Bash aliases shared between zsh and bashrc config.
 
+# Commands
 alias zshrc="$EDITOR ~/dot_files/_zshrc"
 alias vimrc="$EDITOR ~/.vim/vimrc"
+
 # Colors the things that are different, win.
 alias gdc="git diff --color-words"
+
+# Directories
+alias cvim="cd ~/.vim"
+alias cdot="cd ~/dot_files"
 
 # Use MacVim for the terminal if it's installed.
 if [ -x /usr/local/bin/mvim ]; then
