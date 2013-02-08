ff408ee8b29119a4f8bb40bc51097f5d04e07c6a
Jeremy Mack <mack@heysparkbox.com>
2013-02-08 14:51:33 -0500

    cdcode handles more directories

--- zsh/cdcode/cdcode.plugin.zsh
+++ zsh/cdcode/cdcode.plugin.zsh
@@ -1,3 +1,14 @@
+# Code Folder
 c() { cd ~/code/$1; }
 _c() { _files -W ~/code -/; }
 compdef _c c
+
+# Checkout Code Folder
+ck() { cd ~/code/checkout_code/$1; }
+_ck() { _files -W ~/code/checkout_code -/; }
+compdef _ck ck
+
+# Sparkbox Folder
+sb() { cd ~/code/sb/$1; }
+_sb() { _files -W ~/code/sb -/; }
+compdef _sb sb
--- zsh/cdsb/cdsb.plugin.zsh
+++
@@ -1,3 +0,0 @@
-sb() { cd ~/code/sb/$1; }
-_sb() { _files -W ~/code/sb -/; }
-compdef _sb sb
