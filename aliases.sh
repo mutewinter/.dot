58270c612099217dcbaf87208dbb0f7978a0df2b
Jeremy Mack <mack@heysparkbox.com>
2013-03-03 12:47:35 -0500

    No more open with dupes

--- aliases.sh
+++ aliases.sh
@@ -19,3 +19,8 @@ alias gdc="git diff --color-words"
 # Directories
 alias cvim="cd ~/.vim"
 alias cdot="cd ~/dot_files"
+
+# Quick way to rebuild the Launch Services database and get rid
+# of duplicates in the Open With submenu.
+# from: http://www.leancrew.com/all-this/2013/02/getting-rid-of-open-with-duplicates/
+alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
