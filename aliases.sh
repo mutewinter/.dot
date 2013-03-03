f046443c1b7812d6304693809964d735f2a61ff6
Jeremy Mack <mack@heysparkbox.com>
2013-03-03 12:47:24 -0500

    We be vundln' and vundln' and vundln'

--- aliases.sh
+++ aliases.sh
@@ -10,6 +10,7 @@ export EDITOR="vim"
 # Commands
 alias zshrc="$EDITOR ~/dot_files/_zshrc"
 alias vimrc="$EDITOR ~/.vim/vimrc"
+alias vundle="$EDITOR ~/.vim/vundle.vim"
 alias ag="ag --smart-case"
 
 # Colors the things that are different, win.
