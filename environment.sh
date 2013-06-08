e00d5501f3fd7ba2c92eacee981f25292840bac4
Jeremy Mack <mack@heysparkbox.com>
2013-06-08 10:54:45 -0400

    fix: load gnu bin files in path for dircolors

--- environment.sh
+++ environment.sh
@@ -3,3 +3,6 @@
 # ---------------------
 
 PATH=$PATH:/usr/local/share/npm/bin
+# Source coreutils in the path first so dircolors is available.
+# We're using https://github.com/trapd00r/LS_COLORS for sweet file-type colors.
+PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
