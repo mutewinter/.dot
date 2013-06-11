5544fbdec2ec9e7e7410de25b04439099cc1d742
Jeremy Mack <mack@heysparkbox.com>
2013-06-10 20:36:57 -0400

    fix: path setup better for homebrew

--- environment.sh
+++ environment.sh
@@ -6,3 +6,7 @@ PATH=$PATH:/usr/local/share/npm/bin
 # Source coreutils in the path first so dircolors is available.
 # We're using https://github.com/trapd00r/LS_COLORS for sweet file-type colors.
 PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+
+# For Homebrew
+PATH=$PATH:/usr/local/sbin
+PATH="/usr/local/bin:$PATH"
