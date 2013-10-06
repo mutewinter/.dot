1f94840148ffbddfb2ad85d923a6a6e53e6a56fe
Jeremy Mack <mack@heysparkbox.com>
2013-10-06 01:16:39 -0400

    feat: two sweet new aliases. mcd and extract

--- aliases.sh
+++ aliases.sh
@@ -41,3 +41,30 @@ alias annotate-rails="annotate --exclude tests,fixtures,factories -p after"
 alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
 
 alias stripwhitespace='find . -not \( -name .svn -prune -o -name .git -prune -name tmp -prune \) -type f -print0 | xargs -0 sed -i '' -E "s/[[:space:]]*$//"'
+
+# From http://alias.sh/extract-most-know-archives-one-command
+function extract () {
+    if [ -f $1 ] ; then
+      case $1 in
+        *.tar.bz2)   tar xjf $1     ;;
+        *.tar.gz)    tar xzf $1     ;;
+        *.bz2)       bunzip2 $1     ;;
+        *.rar)       unrar e $1     ;;
+        *.gz)        gunzip $1      ;;
+        *.tar)       tar xf $1      ;;
+        *.tbz2)      tar xjf $1     ;;
+        *.tgz)       tar xzf $1     ;;
+        *.zip)       unzip $1       ;;
+        *.Z)         uncompress $1  ;;
+        *.7z)        7z x $1        ;;
+        *)     echo "'$1' cannot be extracted via extract()" ;;
+         esac
+     else
+         echo "'$1' is not a valid file"
+     fi
+}
+
+# From http://alias.sh/make-and-cd-directory
+function mcd() {
+  mkdir -p "$1" && cd "$1";
+}
