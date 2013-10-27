b83a855158d22eb81dac0842cbaab6112a052706
Jeremy Mack <mack@heysparkbox.com>
2013-10-27 15:27:08 -0400

    fix: argument handling for mvimf

--- aliases.sh
+++ aliases.sh
@@ -2,7 +2,7 @@
 
 # Use MacVim for the terminal if it's installed.
 if [ -x /usr/local/bin/mvim ]; then
-  function mvimf() { mvim -v }
+  function mvimf() { mvim -v "$@" }
   export EDITOR=mvimf
   alias view="mvim -v"
   alias vim=mvimf
