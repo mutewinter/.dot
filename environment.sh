dbadc0611ad2ec39e252e76293c2c8ab638432c8
Jeremy Mack <mack@heysparkbox.com>
2013-11-09 16:33:03 -0500

    feat: add startup message with tmux and host info

--- environment.sh
+++ environment.sh
@@ -23,6 +23,17 @@ if [ -f ~/.dot/system_environment.sh ]; then
   source ~/.dot/system_environment.sh
 fi
 
+# ---------------
+# Startup Message
+# ---------------
+hostname
+
+if hash tmux 2>/dev/null; then
+  echo '\ntmux sessions'
+  echo '-------------'
+  tmux list-sessions -F " #{?session_attached,⚙, } #S"
+fi
+
 PATH=$PATH:/usr/local/share/npm/bin
 # Source coreutils in the path first so dircolors is available.
 # We're using https://github.com/trapd00r/LS_COLORS for sweet file-type colors.
