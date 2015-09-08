8efc8d176aed4bcee40161ecd4317fb50c91aac9
Jeremy Mack <jeremy@pileofturtles.com>
2015-09-08 16:45:46 -0500

    chore: remove dead gifify in favor of Gif Brewery

--- functions.sh
+++ functions.sh
@@ -24,21 +24,3 @@ function extract () {
 function mcd() {
   mkdir -p "$1" && cd "$1";
 }
-
-# Sweet Gif Creation from .mov
-#
-# Adapted from https://gist.github.com/SlexAxton/4989674
-gifify() {
-  if [[ -n "$1" ]]; then
-    GIF="${1%.*}.gif"
-    if [[ $2 == '--bad' ]]; then
-      ffmpeg -i $1 -s 600x400 -pix_fmt rgb24 -r 10 -f gif - | gifsicle --optimize=3 --delay=3 > $GIF
-    else
-      ffmpeg -i $1 -r 10 -vcodec png out-static-%05d.png
-      time convert -verbose +dither -layers Optimize -resize 600x600\> out-static*.png  GIF:- | gifsicle --colors 128 --delay=5 --loop --optimize=3 --multifile - > $GIF
-      rm out-static*.png
-    fi
-  else
-    echo "proper usage: gifify <input_movie.mov>. You DO need to include extension."
-  fi
-}
