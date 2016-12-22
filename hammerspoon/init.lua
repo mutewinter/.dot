918667eb19a6bb24d5708f9e8fc3ef0ad5a8be2b
Jeremy Mack <hi@mutewinter.com>
2016-12-22 10:00:18 -0600

    feat: wincent's amazing window movement

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -5,6 +5,11 @@
 -- Disable animation for window resizing so it's instant.
 hs.window.animationDuration = 0
 
+-- allows us to place on quarters, thirds and halves
+hs.grid.setGrid('12x12')
+hs.grid.MARGINX = 0
+hs.grid.MARGINY = 0
+
 -- Global boolean to track triggered state of Hyper.
 local hyperTriggered = false
 
@@ -46,80 +51,108 @@ end)
 -- Window Resizing and Movement
 -- ----------------------------
 
--- HYPER+;: for left one half window
-k:bind({}, ';', function()
-  if hs.window.focusedWindow() then
-    local win = hs.window.focusedWindow()
-    local f = win:frame()
+-- From Wincent's config: https://git.io/v1jJD
+-- Chain the specified movement commands.
+--
+-- This is like the "chain" feature in Slate, but with a couple of enhancements:
+--
+--  - Chains always start on the screen the window is currently on.
+--  - A chain will be reset after 2 seconds of inactivity, or on switching from
+--    one chain to another, or on switching from one app to another, or from one
+--    window to another.
+--
+local lastSeenChain = nil
+local lastSeenWindow = nil
+
+local chain = (function(movements)
+  local chainResetInterval = 2 -- seconds
+  local cycleLength = #movements
+  local sequenceNumber = 1
+  local lastSeenAt = 0
+
+  return function()
+    local win = hs.window.frontmostWindow()
+    local id = win:id()
+    local now = hs.timer.secondsSinceEpoch()
     local screen = win:screen()
-    local max = screen:frame()
 
-    f.x = max.x
-    f.y = max.y
-    f.w = max.w / 2
-    f.h = max.h
-    win:setFrame(f)
-  else
-    hs.alert.show('No active window')
-  end
-  hyperTriggered = true
-end)
+    if
+      lastSeenChain ~= movements or
+      lastSeenAt < now - chainResetInterval or
+      lastSeenWindow ~= id
+    then
+      sequenceNumber = 1
+      lastSeenChain = movements
+    elseif (sequenceNumber == 1) then
+      -- At end of chain, restart chain on next screen.
+      screen = screen:next()
+    end
+    lastSeenAt = now
+    lastSeenWindow = id
 
--- HYPER+': for right one half window
-k:bind({}, '\'', function()
-  if hs.window.focusedWindow() then
-    local win = hs.window.focusedWindow()
-    local f = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
-
-    f.x = max.x + (max.w / 2)
-    f.y = max.y
-    f.w = max.w / 2
-    f.h = max.h
-    win:setFrame(f)
-  else
-    hs.alert.show('No active window')
-  end
-  hyperTriggered = true
-end)
-
--- HYPER+o: Full screen.
-k:bind({}, 'o', function()
-  if hs.window.focusedWindow() then
-    local win = hs.window.focusedWindow()
-    local f = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
-
-    f.x = max.x
-    f.y = max.y
-    f.w = max.w
-    f.h = max.h
-    win:setFrame(f)
-  else
-    hs.alert.show('No active window')
-  end
-  hyperTriggered = true
-end)
-
--- HYPER+i: Center on screen.
-k:bind({}, 'i', function()
-  if hs.window.focusedWindow() then
-    local win = hs.window.focusedWindow()
-    local f = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
-
-    f.x = (max.w / 3) / 2
-    f.y = max.y
-    f.w = max.w - (max.w / 3)
-    f.h = max.h
-    win:setFrame(f)
-  else
-    hs.alert.show('No active window')
+    hs.grid.set(win, movements[sequenceNumber], screen)
+    sequenceNumber = sequenceNumber % cycleLength + 1
   end
 end)
+
+local grid = {
+  topHalf = '0,0 12x6',
+  topThird = '0,0 12x4',
+  topTwoThirds = '0,0 12x8',
+  rightHalf = '6,0 6x12',
+  rightThird = '8,0 4x12',
+  rightTwoThirds = '4,0 8x12',
+  bottomHalf = '0,6 12x6',
+  bottomThird = '0,8 12x4',
+  bottomTwoThirds = '0,4 12x8',
+  leftHalf = '0,0 6x12',
+  leftThird = '0,0 4x12',
+  leftTwoThirds = '0,0 8x12',
+  topLeft = '0,0 6x6',
+  topRight = '6,0 6x6',
+  bottomRight = '6,6 6x6',
+  bottomLeft = '0,6 6x6',
+  fullScreen = '0,0 12x12',
+  centeredBig = '2,0 8x12',
+  centeredSmall = '2,2 8x8',
+}
+
+k:bind({}, ';', chain({
+  grid.leftHalf,
+  grid.leftThird,
+  grid.leftTwoThirds,
+}))
+
+k:bind({}, '\'', chain({
+  grid.rightHalf,
+  grid.rightThird,
+  grid.rightTwoThirds,
+}))
+
+k:bind({}, 'up', chain({
+  grid.topHalf,
+  grid.topThird,
+  grid.topTwoThirds,
+}))
+
+k:bind({}, 'down', chain({
+  grid.bottomHalf,
+  grid.bottomThird,
+  grid.bottomTwoThirds,
+}))
+
+k:bind({}, 'i', chain({
+  grid.topLeft,
+  grid.topRight,
+  grid.bottomRight,
+  grid.bottomLeft,
+}))
+
+k:bind({}, 'o', chain({
+  grid.fullScreen,
+  grid.centeredBig,
+  grid.centeredSmall,
+}))
 
 -- -----------
 -- Hyper Setup
