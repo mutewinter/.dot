4b9be6adba1be238de22a2b2e9f3f75b0ac9054f
Jeremy Mack <mutewinter@users.noreply.github.com>
2022-06-12 12:51:25 -0500

    chore: remove tons of dead aliases

--- aliases.sh
+++ aliases.sh
@@ -1,19 +1,12 @@
 # Bash aliases shared between zsh and bashrc config.
 
-# Use MacVim for the terminal if it's installed.
+# Use Neovim for editing
 if [ -x /usr/local/bin/nvim ]; then
   export EDITOR=nvim
   alias view="nvim -v"
   alias vim=nvim
   alias vimdiff="nvim -vd"
   alias gvim=nvim
-elif [ -x /usr/local/bin/mvim ]; then
-  function mvimf() { mvim -v "$@" }
-  export EDITOR=mvimf
-  alias view="mvim -v"
-  alias vim=mvimf
-  alias vimdiff="mvim -vd"
-  alias gvim=mvim
 else
   export EDITOR="vim"
 fi
@@ -42,8 +35,6 @@ alias gdc="git diff --color-words"
 alias glpp="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias glppf="git log -p --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
-# Annotate Rails models
-alias annotate-rails="annotate --exclude tests,fixtures,factories -p after"
 # Lazygit
 alias lg="lazygit"
 
@@ -52,16 +43,8 @@ alias lg="lazygit"
 # from: http://www.leancrew.com/all-this/2013/02/getting-rid-of-open-with-duplicates/
 alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
 
-# Tapas with Ember
-alias cs='cake server'
-
-# Ember CLI
-alias e=ember
-
 # NPM
 alias n=npm
-alias nide='npm i --save-dev --save-exact'
-alias nie='npm i --save --save-exact'
 
 # Tmux
 alias tm="tmux attach || tmux new"
