418ee3bcbfe9c4667460a10fef0f47b1386da4af
Jeremy Mack <mutewinter@users.noreply.github.com>
2022-06-24 12:52:30 -0500

    refactor: greatly simplify zsh config

--- environment.sh
+++ environment.sh
@@ -39,20 +39,15 @@ if { hash tmux 2>/dev/null; } && ! { [ -n "$TMUX" ]; } then
   fi
 fi
 
-PATH=$PATH:/usr/local/share/npm/bin
-# Source coreutils in the path first so dircolors is available.
-# We're using https://github.com/trapd00r/LS_COLORS for sweet file-type colors.
-PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
-
-# For Homebrew
-PATH=$PATH:/usr/local/sbin
-PATH="/usr/local/bin:$PATH"
-
-# My own programs
-PATH=$PATH:~/programs
-
 # What a world
 export SCARF_ANALYTICS=false
 
 # Disable automatic upgrade of Homebrew packages
 HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
+
+# Use opt location for Homebrew first (from ARM Mac)
+if [ -d /opt/homebrew/bin ]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+else
+  eval "$(/usr/local/bin/brew shellenv)"
+fi
