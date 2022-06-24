085e12fe24aaeb4d491e5286257fa8a0e7916a37
Jeremy Mack <mutewinter@users.noreply.github.com>
2022-06-24 12:56:12 -0500

    feat: syntax highlighting for less

--- fish/conf.d/zz_10_env.fish
+++ fish/conf.d/zz_10_env.fish
@@ -6,3 +6,13 @@ end
 set -gx SCARF_ANALYTICS "false"
 
 set -gx HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK 1
+
+# Via https://github.com/paulirish/dotfiles/blob/main/fish/config.fish
+# highlighting inside manpages and elsewhere
+set -gx LESS_TERMCAP_mb \e'[01;31m'       # begin blinking
+set -gx LESS_TERMCAP_md \e'[01;38;5;74m'  # begin bold
+set -gx LESS_TERMCAP_me \e'[0m'           # end mode
+set -gx LESS_TERMCAP_se \e'[0m'           # end standout-mode
+set -gx LESS_TERMCAP_so \e'[38;5;246m'    # begin standout-mode - info box
+set -gx LESS_TERMCAP_ue \e'[0m'           # end underline
+set -gx LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
