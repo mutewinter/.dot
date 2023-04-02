b2e45cada74693d06255f3b63c0ff892d72c4efe
Jeremy Mack <mutewinter@users.noreply.github.com>
2023-04-02 07:27:09 -0500

    fix: hide node version messages from fnm

--- fish/conf.d/fnm.fish
+++ fish/conf.d/fnm.fish
@@ -1,1 +1,3 @@
-fnm env --use-on-cd | source
+# Log level ensures no "Using Node v***" message is printed (which can intefer
+# with the output of CLI commands, like CtrlSF.vim)
+fnm env --log-level=error --use-on-cd | source
