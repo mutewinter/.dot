ebc79656844c77324ce3afd8244ddb0f5fd0d472
Jeremy Mack <mutewinter@users.noreply.github.com>
2026-02-18 14:42:16 -0600

    feat(fish): pnpm setup

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,8 @@
 set -U fish_autosuggestion_enabled 1
+
+# pnpm
+set -gx PNPM_HOME "/Users/mytop/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
