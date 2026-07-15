6d62ef6b9cd0b8782d4127aaabc282d2c6179f72
Jeremy Mack <mutewinter@users.noreply.github.com>
2026-07-15 11:01:43 -0500

    fish: add ~/.local/bin to PATH
    fixes pnpm-shell-completion fisher plugin: binary installs there but
    nothing put the dir on PATH

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,5 +1,11 @@
 set -U fish_autosuggestion_enabled 1
 
+# ~/.local/bin
+if not string match -q -- "$HOME/.local/bin" $PATH
+  set -gx PATH "$HOME/.local/bin" $PATH
+end
+# ~/.local/bin end
+
 # pnpm
 set -gx PNPM_HOME "$HOME/Library/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
